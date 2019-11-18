c358f19dd17e5498dcd933e06c961103c1ef6587
nohestmm <nohestmm@gmail.com>
2019-11-18 17:59:31 -0300

    adding hooks to componente IconStatus

--- todolist-app/src/components/GridList.jsx
+++ todolist-app/src/components/GridList.jsx
@@ -11,39 +11,41 @@ const GridList = () => {
     const [data, setData] = useState([])
     const [error, setError] = useState('')
 
-    useEffect( () => {
-       PromiseJson('http://my-json-server.typicode.com/nohestmm/json-db/todolist')
-            .then(response => setData(response))
-            .catch(error=>{
+    useEffect(() => {
+        PromiseJson('http://my-json-server.typicode.com/nohestmm/json-db/todolist')
+            .then(response => {
+                setData(response)
+            })
+            .catch(error => {
                 setError(error.message)
             })
-    },[])
+    }, [])
 
-if(error){
-    return (<p>{error}</p>)
-}
-else{
-    return ( 
-        
-        <div>
-            {
-            data.length===0
-            ? <p><img src={loading} alt="loading"/></p>
-            :
-                data.map((el, index) =>
-                    <div key={index}>
-                        <input type="checkbox" name={el.id} id={el.id} />
-                        <span>${el.description}</span>
-                        <input type="text" name="" id="" placeholder={el.date} />
-                        <FontAwesomeIcon icon={faCalendarAlt} />
-                        <IconStatus status={el.state} />
-                    </div>
+    if (error) {
+        return (<p>{error}</p>)
+    }
+    else {
+        return (
 
-                )
-            }
-        </div>
-    )
-        }
+            <div>
+                {
+                    data.length === 0
+                        ? <p><img src={loading} alt="loading" /></p>
+                        :
+                        data.map((el, index) =>
+                            <div key={index}>
+                                <input type="checkbox" name={el.id} id={el.id} />
+                                <span>${el.description}</span>
+                                <input type="text" name="" id="" placeholder={el.date} />
+                                <FontAwesomeIcon icon={faCalendarAlt} />
+                                <IconStatus status={el.state} />
+                            </div>
+
+                        )
+                }
+            </div>
+        )
+    }
 }
 
 
--- todolist-app/src/components/IconStatus.jsx
+++ todolist-app/src/components/IconStatus.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
 import { faClock } from '@fortawesome/free-solid-svg-icons'
 import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'
 
 const IconStatus = ({ status }) => {
-    let icon
-    if (status === "pendiente")
-        icon = <FontAwesomeIcon icon={faClock} />
-    if (status === "atrasada")
-        icon = <FontAwesomeIcon icon={faTimesCircle} />
-    if (status === "liberada")
-        icon = <FontAwesomeIcon icon={faCheckCircle} />
+    const [icon, setIcon] = useState('')
+    useEffect(() => {
+        if (status === "pendiente")
+            setIcon(<FontAwesomeIcon icon={faClock} />)
+        if (status === "atrasada")
+            setIcon(<FontAwesomeIcon icon={faTimesCircle} />)
+        if (status === "liberada")
+            setIcon(<FontAwesomeIcon icon={faCheckCircle} />)
+    })
+
     return (
         <span>
             {icon}
